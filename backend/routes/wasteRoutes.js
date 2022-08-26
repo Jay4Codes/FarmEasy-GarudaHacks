6691973b77fc6e52de7d43af5e8bcd9f331af9f7
JainamShah-22 <jainams6160@gmail.com>
2022-08-26 21:33:06 +0530

    fixed the waste routes and schema

--- backend/routes/wasteRoutes.js
+++ backend/routes/wasteRoutes.js
@@ -4,13 +4,17 @@ const wastes = require('../models/waste');
 const fetchFarmer = require('../middleware/fetchFarmer');
 
 router.post('/postwaste', 
-    fetchFarmer, 
+    // fetchFarmer, 
     async(req, res)=>{
         try {
             const {name, quantity} = req.body
-            const waste = await wastes.findOne({name  : name})
-            const temp = waste.quantity + quantity
-            await wastes.findOneAndUpdate({name  : name}, {quantity : temp})
+            console.log(name);
+            const waste = await wastes.findOne({name : name})
+            console.log(waste);
+            console.log(waste.quantity);
+            // const temp = waste.quantity + quantity
+            await wastes.findOneAndUpdate({name  : name}, {quantity : quantity})
+            res.send("Success")
         } catch (err) {
             console.log(err);
         }
