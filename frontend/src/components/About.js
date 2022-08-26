f15fb9b6ee8e3a63f70866482297090b833ff99b
Jay Jain <jay4codes@gmail.com>
2022-08-26 21:28:14 +0530

    images, contact, about and home page added

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -6,7 +6,90 @@ function About() {
   return (
     <div>
       <Header />
-      About
+      <section className="about section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img className="img-responsive" src="images/about/about.jpg" />
+            </div>
+            <div className="col-md-6">
+              <h2 className="mt-40">What We Do?</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+                enim, accusantium repellat ex autem numquam iure officiis facere
+                vitae itaque.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam
+                qui vel cupiditate exercitationem, ea fuga est velit nulla culpa
+                modi quis iste tempora non, suscipit repellendus labore
+                voluptatem dicta amet?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam
+                qui vel cupiditate exercitationem, ea fuga est velit nulla culpa
+                modi quis iste tempora non, suscipit repellendus labore
+                voluptatem dicta amet?
+              </p>
+              <a href="contact.html" className="btn btn-main mt-20">
+                Watch Our Presentation
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="team-members ">
+        <div className="container">
+          <div className="row">
+            <div className="title">
+              <h2>Team Members</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-3">
+              <div className="team-member text-center">
+                <img className="img-circle" src="images/team/team-1.jpg" />
+                <h4>Hetvi Solanki</h4>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="team-member text-center">
+                <img className="img-circle" src="images/team/team-2.jpg" />
+                <h4>Jay Jain</h4>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="team-member text-center">
+                <img className="img-circle" src="images/team/team-3.jpg" />
+                <h4>Jay Aslaliya</h4>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="team-member text-center">
+                <img className="img-circle" src="images/team/team-1.jpg" />
+                <h4>Jainam Shah</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="section video-testimonial bg-1 overly-white text-center mt-50">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h2>Video presentation</h2>
+              <a
+                className="play-icon"
+                href="https://www.youtube.com/watch?v=oyEuk8j8imI&amp;rel=0"
+                data-toggle="lightbox"
+              >
+                <i className="tf-ion-ios-play"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
