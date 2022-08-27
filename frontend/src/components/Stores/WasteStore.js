2e1c21c4061af9555a71ecd5d366184329caf9ac
Jay Jain <jay4codes@gmail.com>
2022-08-28 03:45:30 +0530

    Modal updated

--- frontend/src/components/Stores/WasteStore.js
+++ frontend/src/components/Stores/WasteStore.js
@@ -7,12 +7,24 @@ import Footer from "../Footer";
 import Header from "../Headers/Header";
 import ScrollToTop from "../ScrollToTop";
 
+// CSS Imports
+import "./Modal.css";
+
 function WasteStore() {
   const [wastes, setWastes] = useState();
   const [loading, setLoading] = useState(true);
 
   const [modal, setModal] = useState(false);
 
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
   useEffect(() => {
     const getWaste = async () => {
       const res = await fetch("http://localhost:5000/api/waste/getwaste", {
@@ -60,11 +72,7 @@ function WasteStore() {
                 </li>
                 <li>
                   <span data-toggle="modal" data-target="#product-modal">
-                    <a
-                      onClick={() => {
-                        setModal(true);
-                      }}
-                    >
+                    <a onClick={toggleModal} className="btn-modal">
                       <i className="tf-ion-android-cart"></i>
                     </a>
                   </span>
@@ -84,42 +92,6 @@ function WasteStore() {
     );
   };
 
-  const itemModal = (props) => {
-    console.log(props);
-    return (
-      <div className="modal product-modal fade" id="product-modal">
-        <button
-          type="button"
-          className="close"
-          data-dismiss="modal"
-          aria-label="Close"
-        >
-          <i className="tf-ion-close"></i>
-        </button>
-        <div className="modal-dialog " role="document">
-          <div className="modal-content">
-            <div className="modal-body">
-              <div className="row">
-                <div className="col-md-8 col-sm-6 col-xs-12">
-                  <div className="modal-image">
-                    <img className="img-responsive" src="" alt="item" />
-                  </div>
-                </div>
-                <div className="col-md-4 col-sm-6 col-xs-12">
-                  <div className="product-short-details">
-                    <h2 className="product-title">asdasd</h2>
-                    <p className="product-price">asd</p>
-                    <input type="number" placeholder="Quantity" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
-
   if (loading) {
     return <div>Loading</div>;
   } else {
@@ -129,11 +101,7 @@ function WasteStore() {
         <Header />
         <ScrollToTop />
         <Fade bottom cascade>
-          {modal ? (
-            <itemModal/>
-          ) : (
-            <></>
-          )}
+          {modal ? <itemModal /> : <></>}
           <section className="products section">
             <div className="container">
               <div className="row">
@@ -152,6 +120,43 @@ function WasteStore() {
             </div>
           </section>
         </Fade>
+
+        {modal && (
+          <div className="modal">
+            <div onClick={toggleModal} className="overlay"></div>
+            <div className="modal product-modal fade" id="product-modal">
+              <button
+                type="button"
+                className="close close-modal"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={toggleModal}
+              >
+                <i className="tf-ion-close"></i>
+              </button>
+              <div className="modal-dialog " role="document">
+                <div className="modal-content">
+                  <div className="modal-body">
+                    <div className="row">
+                      <div className="col-md-8 col-sm-6 col-xs-12">
+                        <div className="modal-image">
+                          <img className="img-responsive" src="" alt="item" />
+                        </div>
+                      </div>
+                      <div className="col-md-4 col-sm-6 col-xs-12">
+                        <div className="product-short-details">
+                          <h2 className="product-title">asdasd</h2>
+                          <p className="product-price">asd</p>
+                          <input type="number" placeholder="Quantity" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
         <Footer />
       </div>
     );
